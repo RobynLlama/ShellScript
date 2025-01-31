5bfadb4eb368919174c2df7bdfeeef0d0a7f2416
Robyn <rnixon98@gmail.com>
2025-01-31 17:07:30 -0600

    refactor: rename property

--- ShellCompiler/Blocks/VariableBlock.cs
+++ ShellCompiler/Blocks/VariableBlock.cs
@@ -5,7 +5,7 @@ namespace ShellCompiler.Blocks;
 public class VariableBlock(string input) : RunnableBlock
 {
   public readonly string Varname = input;
-  public override string RawValue { get => Varname; }
+  public override string ExpressionValue { get => Varname; }
   public override string GetParsedValue(ShellExecutable shell) =>
     shell.GetVariable(Varname).GetValueForTerminal();
 
