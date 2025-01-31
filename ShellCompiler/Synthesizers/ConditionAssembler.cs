4f9587c51d86ae06f3b292421bd6841fcacd3e6b
Robyn <rnixon98@gmail.com>
2025-01-31 13:38:08 -0600

    feat: expression synthesizers

--- ShellCompiler/Synthesizers/ConditionAssembler.cs
+++ ShellCompiler/Synthesizers/ConditionAssembler.cs
@@ -1,96 +1,39 @@
 using ShellCompiler.Statements;
 using ShellCompiler.Blocks;
+using org.matheval;
 
 namespace ShellCompiler;
 
 public static partial class Synthesizers
 {
-  public static IConditional AssembleConditional(Queue<IToken> tokens)
+  public static Expression AssembleConditionExpression(Queue<IToken> tokens)
   {
     //Condition needs
     //  open_parenthesis
-    //  Left hand literal
-    //  one of: ==, >=, <=, >, <
-    //  Right hand literal
     //  close_parenthesis
 
-    RunnableBlock? left = null;
-    ReservedSymbol symbol = ReservedSymbol.STATEMENT_TERMINATOR;
-    RunnableBlock? right = null;
-    bool par_closed = false;
-    bool par_opened = false;
+    if (tokens.Count == 0)
+      throw new InvalidOperationException("Unexpected end of file in AssembleConditionExpression");
+
+    IToken next;
+
+    next = tokens.Dequeue();
+
+    if (next is not Operator op || op.Symbol != ReservedSymbol.PARENTHESIS_OPEN)
+      throw new InvalidOperationException($"Open parenthesis expected in AssembleConditionExpression. Block: {next}");
+
+    Queue<IToken> expressionTokens = new();
 
     while (tokens.Count > 0)
     {
+      next = tokens.Dequeue();
 
-      var current = tokens.Dequeue();
-      //Console.WriteLine(current.GetType().Name);
-
-      /*
-      if (current is KeywordBlock ckw)
-      {
-        Console.WriteLine(ckw.Symbol);
-      }
-      */
-
-      if (!par_opened)
-      {
-        if (current is not ISymbol kw || kw.Symbol != ReservedSymbol.PARENTHESIS_OPEN)
-          throw new InvalidOperationException($"Expecting an open parenthesis while building conditional. Block: {current.GetType().Name}");
-
-        par_opened = true;
-        continue;
-      }
-
-      if (left is null)
-      {
-        if (current is not RunnableBlock lw)
-          throw new InvalidOperationException($"Expected literal as left hand term while building conditional. Block: {current.GetType().Name}");
-
-        left = lw;
-        continue;
-      }
-
-      if (symbol == ReservedSymbol.STATEMENT_TERMINATOR)
-      {
-        if (current is not ISymbol kw)
-          throw new InvalidOperationException($"Expected keyword while building conditional. Block: {current.GetType().Name}");
-
-        symbol = kw.Symbol;
-        continue;
-      }
-
-      if (right is null)
-      {
-        if (current is not RunnableBlock lw)
-          throw new InvalidOperationException($"Expected literal as right hand term while building conditional. Block: {current.GetType().Name}");
-
-        right = lw;
-        continue;
-      }
-
-      if (!par_closed)
-      {
-        if (current is not ISymbol kw || kw.Symbol != ReservedSymbol.PARENTHESIS_CLOSE)
-          throw new InvalidOperationException($"Expecting an close parenthesis while building conditional. Block: {current.GetType().Name}");
-
-        par_closed = true;
+      if (next is Operator op2 && op2.Symbol == ReservedSymbol.PARENTHESIS_CLOSE)
         break;
-      }
 
+      expressionTokens.Enqueue(next);
     }
 
-    if (left is null || symbol == ReservedSymbol.STATEMENT_TERMINATOR || right is null || !par_closed)
-    {
-      throw new InvalidOperationException("Unexpected end of file while building conditional");
-    }
-
-    switch (symbol)
-    {
-      case ReservedSymbol.COMPARISON:
-        return new EqualityStatement(left, right);
-      default:
-        throw new InvalidOperationException($"Invalid symbol in conditional {symbol}");
-    }
+    return AssembleExpressionString(expressionTokens);
   }
 }
---
+++ ShellCompiler/Synthesizers/ExpressionAssembler.cs
@@ -0,0 +1,60 @@
+using System.Text;
+using org.matheval;
+using ShellCompiler.Blocks;
+
+namespace ShellCompiler;
+
+public static partial class Synthesizers
+{
+  public static Expression AssembleExpressionString(Queue<IToken> tokens)
+  {
+    //Expression assembles:
+    //  -No  keywords
+    //  +Yes Literals
+    //  +Yes Variables
+    //  +Yes Operators
+    //  Stops at: Terminator
+
+    if (tokens.Count == 0)
+      throw new InvalidOperationException("Unexpected end of file in AssembleExpressionString");
+
+    StringBuilder fullExpression = new();
+    char delimiter = ' ';
+    bool anything = false;
+    IToken next;
+
+    void AddToExpression(string item)
+    {
+      fullExpression.Append(delimiter);
+      fullExpression.Append(item);
+      anything = true;
+    }
+
+    while (tokens.Count > 0)
+    {
+      next = tokens.Peek();
+
+      if (next is Keyword kw)
+        throw new InvalidOperationException($"Unexpected Keyword in AssembleExpressionString {kw}");
+
+      if (next is RunnableBlock rb)
+      {
+        AddToExpression(rb.RawValue);
+      }
+      else if (next is Operator op)
+      {
+        if (op.Symbol == ReservedSymbol.STATEMENT_TERMINATOR)
+          break;
+
+        AddToExpression(op.ExpressionText);
+      }
+    }
+
+    if (!anything)
+      throw new InvalidOperationException("Empty expression in AssembleExpressionString");
+
+    Console.WriteLine(fullExpression);
+
+    return new(fullExpression.ToString());
+  }
+}
